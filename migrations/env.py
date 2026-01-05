734afc8a15d83d0ef5871b0ff3eb5c6107d6c084
erarunkushwaha <er.arunkushwaha@gmail.com>
2026-01-05 21:29:23 +0545

    env done

--- migrations/env.py
+++ migrations/env.py
@@ -6,8 +6,6 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
-from src.auth.model import User
-from src.books.models import Book
 from sqlmodel import SQLModel
 from src.config import Config
 
